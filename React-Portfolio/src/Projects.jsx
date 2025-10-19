c5b5f7097b4571f1570e135d8c7ba68014b22c36
Samuel Molla <sambenmayu@gmail.com>
2025-10-18 21:30:10 -0700

    Add a project

--- React-Portfolio/src/Projects.jsx
+++ React-Portfolio/src/Projects.jsx
@@ -16,7 +16,20 @@ function Projects() {
       <a href="https://github.com/Mayufisha/Job-Application-Tracker" target="_blank" className="border border-emerald-500 text-emerald-400 hover:bg-emerald-500 hover:text-white font-medium py-1 px-4 rounded transition">GitHub</a>
     </div>
   </div>
-</div>
+    </div>
+
+      <div className="bg-slate-800 rounded-lg shadow-lg hover:shadow-2xl hover:scale-105 hover:-translate-y-1 transform transition duration-300 ease-in-out overflow-hidden">
+        <img src="https://picsum.photos/200/300" alt="Little Lemon Restaurant" className="w-full h-48 object-cover"/>
+        <div className="p-6">
+        <h3 className="text-xl font-semibold mb-2">A Personal Portfolio</h3>
+        <p className="text-gray-300 mb-4">A responsive personal portfolio showcasing projects, skills, and contact info with smooth navigation — built with HTML, Tailwind CSS, and JavaScript.</p>
+
+          <div className="flex flex-wrap gap-4">
+            <a href="https://mayufisha.github.io/Portfolio-2.0" target="_blank" className="bg-emerald-500 hover:bg-emerald-600 text-white font-medium py-1 px-4 rounded transition">Live Demo</a>
+            <a href="https://github.com/Mayufisha/Portfolio-2.0" target="_blank" className="border border-emerald-500 text-emerald-400 hover:bg-emerald-500 hover:text-white font-medium py-1 px-4 rounded transition">GitHub</a>
+          </div>
+        </div>
+      </div>
 
       <div className="bg-slate-800 rounded-lg shadow-lg hover:shadow-2xl hover:scale-105 hover:-translate-y-1 transform transition duration-300 ease-in-out overflow-hidden">
         <img src="/Images/little-lemon-preview.png" alt="Little Lemon Restaurant" className="w-full h-48 object-cover"/>
